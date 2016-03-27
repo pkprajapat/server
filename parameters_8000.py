1128e2059ff1298f4d312dae851785be02076f8c
pkprajapat <prajapatpankajkumar@gmail.com>
2016-03-27 12:04:08 +0530

    just added basic tables

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$972ae7f968ff11f1$3ecfd37861e1b74fb7d7fb14fcb2b663236accc6"
+password="pbkdf2(1000,20,sha512)$bdf68151ecef890a$910acd28bdd049cb3ffed84379bab25f788acff5"
